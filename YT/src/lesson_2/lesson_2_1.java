ee8acd41c7c3010d9dc03425402aa8409c67fbf1
L1ghtsitte <150926079+L1ghtsitte@users.noreply.github.com>
2024-02-13 19:27:28 +0300

    Add files via upload

---
+++ YT/src/lesson_2/lesson_2_1.java
@@ -0,0 +1,9 @@
+package lesson_2;
+
+public class lesson_2_1 {
+	public static void main(String[] args) {
+		System.out.println("Привет.");
+		System.out.println("Я Никита.");
+	}
+
+}
---
+++ YT/src/lesson_2/lesson_2_2.java
@@ -0,0 +1,29 @@
+package lesson_2;
+
+public class lesson_2_2 {
+	public static void main(String[] args) {
+		boolean r = true;
+		byte b = 55;
+		char ch = 'B';
+		char cc = 70;
+		short sh = -15674;
+		int x = -35676129;
+		long l = 99671854L;
+		double d = 44.6;
+		float f = 89.1f;
+		
+		
+		
+		System.out.println(r);
+		System.out.println(b);
+		System.out.println(ch);
+		System.out.println(cc);
+		System.out.println(sh);
+		System.out.println(x);
+		System.out.println(l);
+		System.out.println(d);
+		System.out.println(f);
+		System.out.println("27601");
+	}
+
+}
